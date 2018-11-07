378e1e33ee7c42dcf0a7ea5c820a3e8850e4e4b2
Germain SIPIERE <gsipiere@gmail.com>
2018-11-07 15:16:12 +0100

    modif gitignore et test

--- AfpaSig/src/main/java/afpa/afpasig/MainApp.java
+++ AfpaSig/src/main/java/afpa/afpasig/MainApp.java
@@ -36,56 +36,56 @@ public class MainApp extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+         launch(args);
         
-//         System.out.println("    _________            _ __    __   ___ \n" +
-//                           "   / ____/ (_)___ ____  (_) /_  / /__/__ \\\n" +
-//                           "  / __/ / / / __ `/ _ \\/ / __ \\/ / _ \\/ _/\n" +
-//                           " / /___/ / / /_/ /  __/ / /_/ / /  __/_/  \n" +
-//                           "/_____/_/_/\\__, /\\___/_/_.___/_/\\___(_)   \n" +
-//                           "          /____/  ____  ___   _____       \n" +
-//                           "  ____  __  __   / __ \\/   | / ___/       \n" +
-//                           " / __ \\/ / / /  / /_/ / /| | \\__ \\        \n" +
-//                           "/ /_/ / /_/ /  / ____/ ___ |___/ /        \n" +
-//                           "\\____/\\__,_/  /_/   /_/  |_/____/         \n" +
-//                           "                                          ");
-//        
-//        
-//        connect();
-//        WebDriver driver = new ChromeDriver();
-//
-//        // Déclaration de la page à consulter
-//        driver.get("https://sig.ville.gouv.fr/adresses/recherche");
-//        
-//
-//        // Find the text input element by its name
-//        WebElement codePostal = driver.findElement(By.id("code_postal"));
-//        WebElement nomCommune = driver.findElement(By.id("nom_commune"));
-//        WebElement numAdresse = driver.findElement(By.id("num_adresse"));
-//        WebElement nomVoie = driver.findElement(By.id("nom_voie"));
-//        WebElement rechercher = driver.findElement(By.id("search_zus_form"));
-//        
-//
-//        // Enter something to search for
-//        codePostal.sendKeys("80000");
-//        nomCommune.sendKeys("Amiens");
-//        numAdresse.sendKeys("3");
-//        nomVoie.sendKeys("gambetta");
-//
-//        // Now submit the form. WebDriver will find the form for us from the element
-//        rechercher.submit();
-//        
-//        //recherche des sorties
-//        WebElement reponse = driver.findElement(By.className("red"));
-//
-//
-//
-//
-//        System.out.println(reponse.isDisplayed());
-//        
-//        
-//        //Close the browser
-//        driver.quit();
+         System.out.println("    _________            _ __    __   ___ \n" +
+                           "   / ____/ (_)___ ____  (_) /_  / /__/__ \\\n" +
+                           "  / __/ / / / __ `/ _ \\/ / __ \\/ / _ \\/ _/\n" +
+                           " / /___/ / / /_/ /  __/ / /_/ / /  __/_/  \n" +
+                           "/_____/_/_/\\__, /\\___/_/_.___/_/\\___(_)   \n" +
+                           "          /____/  ____  ___   _____       \n" +
+                           "  ____  __  __   / __ \\/   | / ___/       \n" +
+                           " / __ \\/ / / /  / /_/ / /| | \\__ \\        \n" +
+                           "/ /_/ / /_/ /  / ____/ ___ |___/ /        \n" +
+                           "\\____/\\__,_/  /_/   /_/  |_/____/         \n" +
+                           "                                          ");
+        
+        
+        connect();
+        WebDriver driver = new ChromeDriver();
+
+        // Déclaration de la page à consulter
+        driver.get("https://sig.ville.gouv.fr/adresses/recherche");
+        
+
+        // Find the text input element by its name
+        WebElement codePostal = driver.findElement(By.id("code_postal"));
+        WebElement nomCommune = driver.findElement(By.id("nom_commune"));
+        WebElement numAdresse = driver.findElement(By.id("num_adresse"));
+        WebElement nomVoie = driver.findElement(By.id("nom_voie"));
+        WebElement rechercher = driver.findElement(By.id("search_zus_form"));
+        
+
+        // Enter something to search for
+        codePostal.sendKeys("80000");
+        nomCommune.sendKeys("Amiens");
+        numAdresse.sendKeys("3");
+        nomVoie.sendKeys("gambetta");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        rechercher.submit();
+        
+        //recherche des sorties
+        WebElement reponse = driver.findElement(By.className("red"));
+
+
+
+
+        System.out.println(reponse.isDisplayed());
+        
+        
+        //Close the browser
+        driver.quit();
     }
 
 }
