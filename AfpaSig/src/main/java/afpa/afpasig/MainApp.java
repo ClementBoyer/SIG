3d5d9438b2b35d68e6b7f8b25403af25f98b4268
Germain SIPIERE <gsipiere@gmail.com>
2018-11-07 11:00:55 +0100

    changement titre fenetre

--- AfpaSig/src/main/java/afpa/afpasig/MainApp.java
+++ AfpaSig/src/main/java/afpa/afpasig/MainApp.java
@@ -22,7 +22,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Sig vers Afpa");
         stage.setScene(scene);
         stage.show();
     }
